3f919d48e4b04b86506fb704b21e1930f8461f7d
andrew <1062708758@qq.com>
2023-12-03 01:21:25 +0800

    chore: support umd

--- vite.config.ts
+++ vite.config.ts
@@ -3,29 +3,50 @@ import solid from 'vite-plugin-solid';
 import uno from 'unocss/vite';
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 
-export default defineConfig({
-  plugins: [
+export default defineConfig(({ mode }) => {
+  const plugins = [
     solid(),
     uno(),
     cssInjectedByJsPlugin(),
-  ],
-  build: {
-    rollupOptions: {
-      input: {
-        'core/index': './src/index.ts',
-        'screen/index': './src/screen.tsx',
+  ];
+
+  if (mode === 'umd') {
+    return {
+      plugins,
+      build: {
+        minify: false,
+        rollupOptions: {
+          input: './src/all.ts',
+          output: {
+            format: 'umd',
+            name: 'index',
+            entryFileNames: 'dist.umd.js',
+          },
+          external: ['@soku-games/core'],
+        },
       },
-      output: [
-        {
-          entryFileNames: '[name].cjs',
-          format: 'cjs',
+    };
+  }
+  return {
+    plugins,
+    build: {
+      rollupOptions: {
+        input: {
+          'core/index': './src/index.ts',
+          'screen/index': './src/screen.tsx',
         },
-        {
-          entryFileNames: '[name].mjs',
-          format: 'esm',
-        },
-      ],
-      external: ['@soku-games/core'],
+        output: [
+          {
+            entryFileNames: '[name].cjs',
+            format: 'cjs',
+          },
+          {
+            entryFileNames: '[name].mjs',
+            format: 'esm',
+          },
+        ],
+        external: ['@soku-games/core'],
+      },
     },
-  },
+  };
 });
